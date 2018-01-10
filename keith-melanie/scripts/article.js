ac8fd12667d883131335f4c1a57f36bfda629be0
Melanie Cohen <melaniebcohen@gmail.com>
2018-01-10 12:05:38 -0800

    working on jquery

--- keith-melanie/scripts/article.js
+++ keith-melanie/scripts/article.js
@@ -22,28 +22,24 @@ Article.prototype.toHtml = function() {
   // PUT YOUR RESPONSE HERE
 
   let $newArticle = $('article.template').clone();
-  /* TODO: This cloned article still has a class of template. In our modules.css stylesheet, we should give all elements with a class of template a display of none so that our template does not display in the browser. But, we also need to make sure we're not accidentally hiding our cloned article. */
+  /* DONE: This cloned article still has a class of template. In our modules.css stylesheet, we should give all elements with a class of template a display of none so that our template does not display in the browser. But, we also need to make sure we're not accidentally hiding our cloned article. */
+
+  $newArticle.removeClass('template');
 
   if (!this.publishedOn) $newArticle.addClass('draft');
   $newArticle.attr('data-category', this.category);
 
-
-  /* TODO: Now use jQuery traversal and setter methods to fill in the rest of the current template clone with values of the properties of this particular Article instance.
+  /* DONE: Now use jQuery traversal and setter methods to fill in the rest of the current template clone with values of the properties of this particular Article instance.
     We need to fill in:
       1. author name,
       2. author url,
       3. article title,
       4. article body, and
       5. publication date. */
-
-   $('h1').replaceWith(this.title);
-   $('a').text(this.author);
-   $('a').attr('href', this.authorUrl);
-   $('.article-body').html(this.body);
-   $('time').attr('datetime', this.publishedOn);
-   $('time').text(this.publishedOn);
-
-
+  $newArticle.find('h1').text(this.title);
+  $newArticle.find('a').text(this.author).attr('href', this.authorUrl);
+  $newArticle.find('section').html(this.body);
+  $newArticle.find('time').attr('datetime', this.publishedOn).text(this.publishedOn);
 
   // REVIEW: Display the date as a relative number of 'days ago'
   $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
@@ -58,10 +54,10 @@ rawData.sort(function(a,b) {
 
 // TODO: Refactor these for loops using the .forEach() array method.
 
-// for(let i = 0; i < rawData.length; i++) {
-//   articles.push(new Article(rawData[i]));
-// }
+for(let i = 0; i < rawData.length; i++) {
+  articles.push(new Article(rawData[i]));
+}
 
-// for(let i = 0; i < articles.length; i++) {
-//   $('#articles').append(articles[i].toHtml());
-// }
+for(let i = 0; i < articles.length; i++) {
+  $('#articles').append(articles[i].toHtml());
+}
