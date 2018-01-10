64d8de136a9ce7760e8b1e4154dbfbcf3574ba89
Melanie Cohen <melaniebcohen@gmail.com>
2018-01-10 12:32:00 -0800

    jQuery and styling

--- keith-melanie/scripts/article.js
+++ keith-melanie/scripts/article.js
@@ -2,8 +2,8 @@
 
 let articles = [];
 
-// COMMENT: What is the purpose of the following function? Why is its name capitalized? Explain the context of "this" within the function. What does "rawDataObj" represent?
-// 
+// DONE: What is the purpose of the following function? Why is its name capitalized? Explain the context of "this" within the function. What does "rawDataObj" represent?
+// The function below is a constructor function that creates objects. The rawDataObj represents an individual object as an argument from the rawData array in the blogArticles.js file. Within the function, "this" is self-referential, in that it represents the value within the instantiated object.
 
 function Article(rawDataObj) {
   // DONE: Use the JS object that is passed in to complete this constructor function:
@@ -14,12 +14,11 @@ function Article(rawDataObj) {
   this.authorUrl = rawDataObj.authorUrl;
   this.publishedOn = rawDataObj.publishedOn;
   this.body = rawDataObj.body;
-  articles.push(this);
 }
 
 Article.prototype.toHtml = function() {
-  // COMMENT: What is the benefit of cloning the article? (see the jQuery docs)
-  // PUT YOUR RESPONSE HERE
+  // DONE: What is the benefit of cloning the article? (see the jQuery docs)
+  // Cloning takes an element on the DOM and literally clones it, as well as its children elements. Using this method, we can get and set entire sections.
 
   let $newArticle = $('article.template').clone();
   /* DONE: This cloned article still has a class of template. In our modules.css stylesheet, we should give all elements with a class of template a display of none so that our template does not display in the browser. But, we also need to make sure we're not accidentally hiding our cloned article. */
@@ -38,26 +37,26 @@ Article.prototype.toHtml = function() {
       5. publication date. */
   $newArticle.find('h1').text(this.title);
   $newArticle.find('a').text(this.author).attr('href', this.authorUrl);
-  $newArticle.find('section').html(this.body);
+  $newArticle.find('section').html('<p>' + this.body + '</p>');
   $newArticle.find('time').attr('datetime', this.publishedOn).text(this.publishedOn);
 
-  // REVIEW: Display the date as a relative number of 'days ago'
+  // DONE: Display the date as a relative number of 'days ago'
   $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
   $newArticle.append('<hr>');
   return $newArticle;
 };
 
 rawData.sort(function(a,b) {
-  // REVIEW: Take a look at this sort method; This may be the first time we've seen it. Look at the docs and think about how the dates would be sorted if the callback were not included in this method.
+  // DONE: Take a look at this sort method; This may be the first time we've seen it. Look at the docs and think about how the dates would be sorted if the callback were not included in this method.
   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
 });
 
-// TODO: Refactor these for loops using the .forEach() array method.
+// DONE: Refactor these for loops using the .forEach() array method.
 
-for(let i = 0; i < rawData.length; i++) {
-  articles.push(new Article(rawData[i]));
-}
+rawData.forEach(function (rawData) {
+  articles.push(new Article(rawData));
+});
 
-for(let i = 0; i < articles.length; i++) {
-  $('#articles').append(articles[i].toHtml());
-}
+articles.forEach(function (articles) {
+  $('#articles').append(articles.toHtml());
+})
